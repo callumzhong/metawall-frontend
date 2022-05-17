ca9f59386f23910a478312bdaa57f43c9a77b1aa
callumzhong <callum.zhong@gmail.com>
2022-05-17 15:11:04 +0800

    feat: 使用 ref 參考 input, 手動觸發 file 測試

--- src/stories/components/postNewFeed/postNewFeed.jsx
+++ src/stories/components/postNewFeed/postNewFeed.jsx
@@ -1,6 +1,8 @@
 import { useRef } from 'react';
 import Button, {
 	ButtonBorderRadius,
+	ButtonColor,
+	ButtonMode,
 	ButtonSize,
 	ButtonTheme,
 } from 'stories/modules/button/Button';
@@ -12,26 +14,46 @@ const PostNewFeed = ({ mode, className, ...props }) => {
 		event.preventDefault();
 	};
 	const fileRef = useRef();
-	const uploadImageHandler = () => {
+	const imageButtonClickHandler = () => {
 		fileRef.current.click();
+	};
+	const uploadImageHandler = (e) => {
+		const file = e.target.files[0];
+		// 處理檔案上傳
+		console.log(file);
 	};
 	return (
 		<Card mode={CardMode['shadow-bottom']} className='p-8'>
 			<form onSubmit={postSubmitHandler}>
-				<label className='mb-4'>
+				<label className='mb-4 block'>
 					<div className='mb-1'>貼文內容</div>
 					<Textarea className='h-[10.5625rem] w-full' />
 				</label>
 				<Button
+					type='button'
 					theme={ButtonTheme.black}
 					size={ButtonSize.sm}
 					borderRadius={ButtonBorderRadius.sm}
-					className='px-8'
-					onClick={uploadImageHandler}
+					className='mb-6 px-8'
+					onClick={imageButtonClickHandler}
 				>
 					上傳圖片
 				</Button>
-				<input ref={fileRef} className='hidden' type='file' />
+				<input
+					ref={fileRef}
+					onChange={uploadImageHandler}
+					className='hidden'
+					type='file'
+				/>
+				<Button
+					type='submit'
+					theme={ButtonTheme.secondary}
+					size={ButtonSize.lg}
+					color={ButtonColor.black}
+					mode={ButtonMode['outline-black']}
+				>
+					送出貼文
+				</Button>
 			</form>
 		</Card>
 	);
