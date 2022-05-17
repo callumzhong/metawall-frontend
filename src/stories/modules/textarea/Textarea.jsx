baa3c3f3ec0a20c80280cc76b925b1757fa8e1f0
callumzhong <callum.zhong@gmail.com>
2022-05-17 13:19:15 +0800

    feat: 新增textarea

--- src/stories/modules/textarea/Textarea.jsx
+++ src/stories/modules/textarea/Textarea.jsx
@@ -1,5 +1,10 @@
 const Textarea = ({ className, ...props }) => {
-	return <textarea className='' {...props} />;
+	return (
+		<textarea
+			className='min-h-[10.5625rem] w-full border-2 border-black py-3 px-4'
+			{...props}
+		/>
+	);
 };
 
 export default Textarea;
