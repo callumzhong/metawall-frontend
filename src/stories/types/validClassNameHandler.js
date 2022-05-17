db71ee5d2cad650bfc30649342461e9c7ef6111b
callumzhong <callum.zhong@gmail.com>
2022-05-17 13:18:57 +0800

    feat: 新增 proptype 自定義驗證

---
+++ src/stories/types/validClassNameHandler.js
@@ -0,0 +1,17 @@
+const validClassNameHandler =
+	(regex = /p|m/, message = 'only p-* m-*') =>
+	(props, propName, componentName) => {
+		const classes = props[propName].split(' ');
+		if (classes.some((i) => !new RegExp(regex).test(i))) {
+			return new Error(
+				'Invalid prop `' +
+					propName +
+					'` supplied to' +
+					' `' +
+					componentName +
+					`Validation failed ${message} `,
+			);
+		}
+	};
+
+export default validClassNameHandler;
